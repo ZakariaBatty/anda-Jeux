3439c96a3d22cad04e4246573a2bc7167ffabfbd
ZakariaBatty <zbatty1297@gmail.com>
2025-02-02 12:45:16 +0100

    Comment out basePath, assetPrefix, and domains in Next.js configuration for improved flexibility in deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type { import('next').NextConfig } */
 const nextConfig = {
    output: 'standalone',
-   basePath: '/quiz',
-   assetPrefix: '/quiz/',
+   // basePath: '/quiz',
+   // assetPrefix: '/quiz/',
    images: {
       unoptimized: true, // Required for LWS cPanel deployment
-      domains: ['https://devloup.com'], // Add your domain here
+      // domains: ['https://devloup.com'], // Add your domain here
       dangerouslyAllowSVG: true,
    },
 };
